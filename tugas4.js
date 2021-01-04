1992acb602a2a0bf04d6a9ab6eb9afcf569ed7e3
Ridwan <71011389+riidwanadiitya@users.noreply.github.com>
2021-01-04 08:52:30 +0700

    TUGAS 4 JS

---
+++ tugas4.js
@@ -0,0 +1,17 @@
+var Ridwan = 166;
+var Adit = 160;
+var Putra = 165;
+
+if (Ridwan < Adit) {
+    if (Ridwan < Putra) {
+        console.log("Ridwan paling pendek");
+    } else {
+        console.log("Ridwan paling pendek kedua");
+    }
+} else {
+    if (Ridwan > Putra) {
+        console.log("Ridwan paling tinggi");
+    } else {
+        console.log("Ridwan paling tinggi kedua");
+    }
+}
